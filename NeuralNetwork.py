2670f9fd10612d98686f7b269132ee5147234b41
Ramesh <sgkrame2@liverpool.ac.uk>
2026-05-21 20:18:33 +0100

    first commit

---
+++ NeuralNetwork.py
@@ -0,0 +1,19 @@
+class NeuralNetwork():
+        # Initialisation
+    def __init__():
+        pass
+
+    # Forward pass
+    def forward(params, X):     # cache stores Z1,A1,Z2 for backprop
+        pass
+    # Backward pass
+    def backward(params, cache, X, Y):
+        pass
+
+    # Update
+    def update(params, grads, lr):
+        pass
+
+    # Training loop
+    def train(X, Y, epochs, lr):
+        pass
